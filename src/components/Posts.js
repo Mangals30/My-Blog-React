4cbf3900cda19da10bba621b3561851b7df48ade
Mangals30 <mangalam301185@gmail.com>
2020-03-20 21:20:11 +0200

    first commit

---
+++ src/components/Posts.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Post from './Post';
+import { Link } from 'react-router-dom';
+
+const Posts = ({posts,deletePost,editPost}) => {
+    const postsList = posts.map((post => 
+        <Post key = {post.id} post = {post} deletePost = {deletePost} editPost = {editPost}/>))
+    
+        return (
+            <div>
+                <div className="header">
+                 <div className="header-wrapper">
+                    <Link to = '/newpost'><button className='addPost-btn'>New Post</button></Link>
+                    <h2>Number of Posts : {posts.length}</h2>
+                </div>
+                </div>
+               
+                   
+                   
+                    <div className='all-post-container'>
+                    {postsList}
+                    </div>
+                  
+            </div>
+        )
+    
+    
+
+}
+
+export default Posts
